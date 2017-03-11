141c46367cc1485c284e956d2f4338869f62b8dd
Dibyendu Majumdar <noemail>
2017-03-11 23:25:12 +0000

    test cases

---
+++ tests/picoc/17_enum.c
@@ -0,0 +1,27 @@
+extern int printf(const char *fmt, ...);
+
+enum fred
+{
+    a,
+    b,
+    c,
+    d,
+    e = 54,
+    f = 73,
+    g,
+    h
+};
+
+int main(void)
+{
+	enum fred frod;
+
+	printf("%d %d %d %d %d %d %d %d\n", a, b, c, d, e, f, g, h);
+	printf("%d\n", frod);
+	frod = 12;
+	printf("%d\n", frod);
+	frod = e;
+	printf("%d\n", frod);
+
+	return 0;
+}

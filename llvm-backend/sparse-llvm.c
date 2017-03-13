29ad2032a84cb3c42040382b7c678d1a1445f1f0
Dibyendu Majumdar <noemail>
2017-03-13 20:33:02 +0000

    op_sub should allow first operand to be a pointer

--- llvm-backend/sparse-llvm.c
+++ llvm-backend/sparse-llvm.c
@@ -538,6 +538,8 @@ static void output_op_binary(struct dmr_C *C, struct function *fn, struct instru
 	case OP_SUB:
 		if (symbol_is_fp_type(C, insn->type))
 			target = LLVMBuildFSub(fn->builder, lhs, rhs, target_name);
+		else if (LLVMGetTypeKind(LLVMTypeOf(lhs)) == LLVMPointerTypeKind)
+			target = calc_gep(C, fn->builder, lhs, rhs);
 		else
 			target = LLVMBuildSub(fn->builder, lhs, rhs, target_name);
 		break;

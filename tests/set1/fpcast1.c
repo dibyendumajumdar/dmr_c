c4d0ab38bb690d5b089204751d26027eecc67108
Dibyendu Majumdar <noemail>
2017-03-17 13:59:31 +0000

    add floating point test case from lcc

--- tests/set1/fpcast1.c
+++ tests/set1/fpcast1.c
@@ -1,5 +1,8 @@
 extern int printf(const char *, ...);
-
+double f1(void) { return -1; }
+  double f2(void) { return (double)-1; }
+  double f3(void) { return -1.0; }
+  
 int main(void)
 {
 	float a = 'a';

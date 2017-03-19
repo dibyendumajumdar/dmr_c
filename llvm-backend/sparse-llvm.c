f85e9d6ff156759e263bcbb81de3387067f9327e
Dibyendu Majumdar <noemail>
2017-03-19 10:02:29 +0000

    cast operands to target type in op_sel

--- llvm-backend/sparse-llvm.c
+++ llvm-backend/sparse-llvm.c
@@ -916,12 +916,14 @@ static void output_op_br(struct dmr_C *C, struct function *fn, struct instructio
 static void output_op_sel(struct dmr_C *C, struct function *fn, struct instruction *insn)
 {
 	LLVMValueRef target, src1, src2, src3;
+	LLVMTypeRef desttype = insn_symbol_type(C, fn->module, insn);
 
 	src1 = bool_value(C, fn, pseudo_to_value(C, fn, insn, insn->src1));
 	src2 = pseudo_to_value(C, fn, insn, insn->src2);
 	src3 = pseudo_to_value(C, fn, insn, insn->src3);
-	/* make sure the two values are same type */
-	//src3 = build_cast(C, fn, src3, LLVMTypeOf(src2), "");
+	/* make sure the two values are cast to target type */
+	src2 = build_cast(C, fn, src2, desttype, "src2");
+	src3 = build_cast(C, fn, src3, desttype, "src3");
 	target = LLVMBuildSelect(fn->builder, src1, src2, src3, "select");
 
 	insn->target->priv = target;

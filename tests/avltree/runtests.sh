3771cd5cafc847cfdadf1bfe5f6cc7d42c61566d
dibyendumajumdar <nomail>
2017-03-22 14:49:44 -0700

    add -O flag to the test drivers

--- tests/avltree/runtests.sh
+++ tests/avltree/runtests.sh
@@ -2,7 +2,7 @@ tests_list=test.c
 
 for f in $tests_list; do
 	echo "running $f"
-	../../build/sparse-llvm $f
+	../../build/sparse-llvm -O1 $f
 	if [ "$?" -ne "0" ]; then
                 echo "$f failed to compile"
 	else
--- tests/lcc/runtests.sh
+++ tests/lcc/runtests.sh
@@ -2,7 +2,7 @@ tests_list=`find . -name '*.c' | sed -e 's#^\./\(.*\)#\1#' | sort`
 
 for f in $tests_list; do
 	echo "running $f"
-	../../build/sparse-llvm $f
+	../../build/sparse-llvm -O1 $f
 	if [ "$?" -ne "0" ]; then
                 echo "$f failed to compile"
 	else

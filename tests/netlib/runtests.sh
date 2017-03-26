db32c70089b66e1d27ef2c60ac4f2a9e9f0e675e
dibyendumajumdar <nomail>
2017-03-26 03:40:39 -0700

    change test scripts to allow -O1 option to be optional and test that results are correct with or without -O1

--- tests/netlib/runtests.sh
+++ tests/netlib/runtests.sh
@@ -1,8 +1,9 @@
+opt=$1
 tests_list=`find . -name '*.c' | sed -e 's#^\./\(.*\)#\1#' | sort`
 
 for f in $tests_list; do
 	echo "running $f"
-	../../build/sparse-llvm -O1 $f
+	../../build/sparse-llvm $opt $f
 	if [ "$?" -ne "0" ]; then
                 echo "$f failed to compile"
 	else

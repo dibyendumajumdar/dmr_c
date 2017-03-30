91ae1b249bc1811e454d8640b512ac6772f6e5ff
Dibyendu Majumdar <noemail>
2017-03-30 09:34:14 +0100

    issue #13 expose the LLVM compilation function as an API and ensure that all code is generated within a given context rather than the global context

---
+++ llvm-backend/dmr_c_llvm.h
@@ -0,0 +1,10 @@
+#ifndef DMR_C_LLVM_H
+#define DMR_C_LLVM_H
+
+#include <llvm-c/Core.h>
+#include <port.h>
+#include <dmr_c.h>
+
+extern LLVMModuleRef dmr_C_llvmcompile(int argc, char **argv, LLVMContextRef context, const char *modulename, const char *inputbuffer);
+
+#endif

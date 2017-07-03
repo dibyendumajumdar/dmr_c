1c6d985f61bdc1f1950f36aae6c56c83b8e5b6f2
Dibyendu Majumdar <noemail>
2017-07-03 18:10:16 +0100

    make test easily runnable

--- nanojit-backend/testnanobackend.c
+++ nanojit-backend/testnanobackend.c
@@ -68,15 +68,65 @@ static int test2(int argc, char **argv)
 	return rc;
 }
 
+static int test3(int argc, char **argv)
+{
+	const char *code = "struct mystruct { "
+			   "double d; "
+			   "int a, b; "
+			   "char array[10]; "
+			   "};\n"
+
+			   "int storeint(struct mystruct *p) "
+			   "{ "
+			   "p->array[0] = 'h'; "
+			   "p->array[1] = 'e'; "
+			   "p->array[2] = 'l'; "
+			   "p->array[3] = 'l'; "
+			   "p->array[4] = 'o'; "
+			   "p->array[5] = 0; "
+			   "p->a = 42; "
+			   "p->b = 96; "
+			   // p->d = 300.42;
+			   "return 0; "
+			   "}\n";
+
+	struct mystruct {
+		double d;
+		int a, b;
+		char array[10];
+	};
+
+	NJXContextRef module = NJX_create_context(true);
+	int rc = 0;
+	if (!dmrC_nanocompile(argc, argv, module, code))
+		rc = 1;
+	int (*fp)(struct mystruct *) = NULL;
+	if (rc == 0) {
+		fp = NJX_get_function_by_name(module, "storeint");
+		if (!fp)
+			rc = 1;
+	}
+	if (rc == 0 && fp) {
+		struct mystruct T;
+		int result = fp(&T);
+		if (result != 0 || strcmp(T.array, "hello") != 0 || T.a != 42 ||
+		    T.b != 96)
+			rc = 1;
+	}
+	NJX_destroy_context(module);
+	return rc;
+}
+
 int main(int argc, char **argv)
 {
 	int rc = 0;
 	rc += test1(argc, argv);
-    rc += test2(argc, argv);
+	rc += test2(argc, argv);
+	rc += test3(argc, argv);
 
-    if (rc == 0)
-        printf("Test OK\n");
-    else
-        printf("Test FAILED\n");
+	if (rc == 0)
+		printf("Test OK\n");
+	else
+		printf("Test FAILED\n");
 	return rc;
 }

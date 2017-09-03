309c3165767fd5a065953654e700c2e45010f0ed
Dibyendu Majumdar <noemail>
2017-09-03 07:26:01 +0100

    Add ability to run tests using the commandline sparse-nano and enhance the test switch.c to make it runnable

--- nanojit-backend/nanobackend.c
+++ nanojit-backend/nanobackend.c
@@ -1,17 +1,34 @@
 #include <nanojitextra.h>
 
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-extern bool dmrC_nanocompile(int argc, char **argv, NJXContextRef module, const char *inputbuffer);
+extern bool dmrC_nanocompile(int argc, char **argv, NJXContextRef module,
+			     const char *inputbuffer);
 
 int main(int argc, char **argv)
 {
 	NJXContextRef module = NJX_create_context(true);
 
-	int rc = 1;
-	dmrC_nanocompile(argc, argv, module, NULL);
+	int rc = 0;
+	if (!dmrC_nanocompile(argc, argv, module, NULL))
+		rc = 1;
 
+	int (*fp)(void) = NULL;
+	if (rc == 0) {
+		/* To help with testing check if the source defined a function
+		 * named TestNano() and if so, execute it
+		 */
+		fp = NJX_get_function_by_name(module, "TestNano");
+		if (fp) {
+			if (fp() != 0) {
+				printf("TestNano Failed\n");
+				rc = 1;
+			} else {
+				printf("TestNano OK\n");
+			}
+		}
+	}
 	NJX_destroy_context(module);
 
 	return rc;

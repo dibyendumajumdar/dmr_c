e0209f8a7c65e208430662bbf3bd58153b947457
Dibyendu Majumdar <noemail>
2017-12-24 01:38:19 +0000

    prepare for merge

--- tests/set1/runtests.sh
+++ tests/set1/runtests.sh
@@ -3,11 +3,11 @@ tests_list=`find . -name '*.c' | sed -e 's#^\./\(.*\)#\1#' | sort`
 
 for f in $tests_list; do
 	echo "running $f"
-	../../build/sparse-llvm $opt $f
+	sparse-llvm $opt $f
 	if [ "$?" -ne "0" ]; then
                 echo "$f failed to compile"
 	else
-		~/LLVM/bin/lli out.bc
+		lli out.bc
 		if [ "$?" -ne "0" ]; then
 			echo "$f failed to run"
 		fi	
